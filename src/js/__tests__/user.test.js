c600a32b46b74458568490b41fbcf1332cfc2740
Job Goog <roro425052@gmail.com>
2024-11-19 04:16:53 +0300

    done test 100%

--- src/js/__tests__/user.test.js
+++ src/js/__tests__/user.test.js
@@ -1,16 +1,31 @@
-import { loadUser } from '../user';
+import { loadUser, saveUser } from '../user';
 import { httpGet } from '../http';
 
 jest.mock('../http');
 
-beforeEach(() => {
-  jest.resetAllMocks();
-});
+describe('User Functions', () => {
+  test('should throw error for saveUser', () => {
+    expect(() => saveUser({ name: 'test' })).toThrow('Unimplemented');
+  });
 
-test('should call loadUser once', () => {
-  httpGet.mockReturnValue(JSON.stringify({}));
+  test('should call httpGet in loadUser', () => {
+    httpGet.mockImplementation((url) => {
+      if (url === 'http://server:8080/users/1') {
+        return JSON.stringify({ id: 1, name: 'John' });
+      }
+      throw new Error('Invalid URL');
+    });
 
-  const response = loadUser(1);
-  expect(response).toEqual({});
-  expect(httpGet).toHaveBeenCalledWith('http://server:8080/users/1');
+    const result = loadUser(1);
+    expect(httpGet).toHaveBeenCalledWith('http://server:8080/users/1');
+    expect(result).toEqual({ id: 1, name: 'John' });
+  });
+
+  test('should handle httpGet throwing an error', () => {
+    httpGet.mockImplementation(() => {
+      throw new Error('Network Error');
+    });
+
+    expect(() => loadUser(1)).toThrow('Network Error');
+  });
 });
